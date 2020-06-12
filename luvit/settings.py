30f872762287b3a381f804aa8d9a34cc50deff7b
Ricardo Manzanares <rmanzvila@gmail.com>
2020-06-12 11:43:48 -0500

    feat: Support RDS remote DB using ENV vars

--- luvit/settings.py
+++ luvit/settings.py
@@ -74,9 +74,13 @@ WSGI_APPLICATION = 'luvit.wsgi.application'
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 # For postgres django.db.backends.postgresql
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': os.environ['RDS_DB_NAME'],
+            'USER': os.environ['RDS_DB_USER'],
+            'PASSWORD': os.environ['RDS_DB_PASS'],
+            'HOST': os.environ['RDS_HOSTNAME'],
+            'PORT': os.environ['RDS_DB_PORT'],
     }
 }
 
